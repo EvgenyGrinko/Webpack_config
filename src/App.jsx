34d3b7e370eaf5cbf2fbaaf622f810113e51b2dd
Evgeny Grinko <john_forever_young@mail.ru>
2021-02-22 16:40:59 +0300

    Add react entry point and simple component for testing

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => {
+    return (
+        <div className={styles.wrapper}>
+            <span className={styles.text}>
+                Hello React Component
+            </span>
+        </div>
+    )
+}
+
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,13 @@ import '@/styles/styles.css';
 import json from '@/assets/json';
 import './scss.scss';
 import './babel';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
 
 const post = new Post('Test title');
 
 console.log(`Post to string: `, post.toString());
-
 console.log(json)
+
+render(<App />, document.getElementById('root'))
