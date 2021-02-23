ad13a476b02862330476e8979d84a9c8a10c40d8
Evgeny Grinko <john_forever_young@mail.ru>
2021-02-23 14:49:59 +0300

    Webpack suports lazy-loading out of the box. Example of usage here.

--- src/babel.js
+++ src/babel.js
@@ -7,4 +7,7 @@ class Utils {
     static field = 'Hello'
 }
 
-console.log(Utils.field)
+console.log(Utils.field);
+import('lodash').then( ({ default: _ }) => {
+    console.log('lodash', _.random(0, 9, true))
+})
